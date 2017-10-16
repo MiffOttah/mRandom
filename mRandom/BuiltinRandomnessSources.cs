4259e7e575a2a4e665795a0e1fc8915229a709ee
Miff Falden <miff@miffthefox.info>
2017-10-16 17:48:54 -0400

    Start on framework for randomness

---
+++ mRandom/BuiltinRandomnessSources.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+
+namespace MiffTheFox.mRandom
+{
+    public class SystemRandomRandomnessSource : IRandomnessSource
+    {
+        private Random _RNG;
+
+        public SystemRandomRandomnessSource()
+        {
+            _RNG = new Random();
+        }
+
+        public SystemRandomRandomnessSource(Random rng)
+        {
+            _RNG = rng;
+        }
+
+        public void Dispose()
+        {
+            _RNG = null;
+        }
+
+        public BinString GetRandomBytes(int length)
+        {
+            byte[] b = new byte[length];
+            _RNG.NextBytes(b);
+            return new BinString(b);
+        }
+    }
+    
+    public class CryptoRandomnessSource : IRandomnessSource
+    {
+        private RandomNumberGenerator _RNG;
+
+        public CryptoRandomnessSource()
+        {
+            _RNG = RandomNumberGenerator.Create();
+        }
+
+        public CryptoRandomnessSource(RandomNumberGenerator rng)
+        {
+            _RNG = rng;
+        }
+
+        public void Dispose()
+        {
+            ((IDisposable)_RNG).Dispose();
+        }
+
+        public BinString GetRandomBytes(int length)
+        {
+            byte[] b = new byte[length];
+            _RNG.GetBytes(b);
+            return new BinString(b);
+        }
+    }
+}
---
+++ mRandom/RandomGenerator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiffTheFox.mRandom
+{
+    public class RandomGenerator : IDisposable
+    {
+        private readonly IRandomnessSource _Source;
+        public IRandomnessSource Source => _Source;
+        
+        public RandomGenerator(IRandomnessSource source)
+        {
+            _Source = source;
+        }
+
+        public void Dispose()
+        {
+            _Source.Dispose();
+        }
+    }
+}
