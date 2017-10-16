b3b1369d3eaf7b81bd3f785c196a40b1fb038ac3
Miff Falden <miff@miffthefox.info>
2017-10-16 18:51:49 -0400

    Some collection stuff

---
+++ mRandom/RandomGeneratorSequence.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiffTheFox.mRandom
+{
+    partial class RandomGenerator
+    {
+        private static IList<T> _CollapseToList<T>(IEnumerable<T> source)
+        {
+            // If source isn't an IList<T>, enumerate it and make it an array
+            if (source is IList<T> sourceList) return sourceList;
+            return source.ToArray();
+        }
+
+        public T Pick<T>(IEnumerable<T> source)
+        {
+            var sourceList = _CollapseToList(source);
+            int choice = RandomLessThan(sourceList.Count);
+            return sourceList[choice];
+        }
+
+        public T[] Shuffle<T>(IEnumerable<T> source)
+        {
+            var sourceList = _CollapseToList(source);
+            int[] selections = Enumerable.Range(0, sourceList.Count).ToArray();
+            int j, temp;
+
+            for (int i = selections.Length - 1; i >= 0; i--)
+            {
+                j = RandomBetweenInclusive(0, i);
+                if (j != i)
+                {
+                    temp = selections[j];
+                    selections[j] = selections[i];
+                    selections[i] = temp;
+                }
+            }
+
+            var result = new T[selections.Length];
+            for (int i = 0; i < result.Length; i++)
+            {
+                result[i] = sourceList[selections[i]];
+            }
+            return result;
+        }
+
+        public T[] SampleWithoutReplacement<T>(IEnumerable<T> source, int samples)
+        {
+            if (samples <= 0) throw new ArgumentException("Must take 1 or more samples.");
+                    
+            var shuffled = Shuffle(source);
+            if (samples > shuffled.Length) throw new InvalidOperationException("Not enough data in the source to take the provided number of samples.");
+            if (samples == shuffled.Length) return shuffled;
+
+            var result = new T[samples];
+            Array.Copy(shuffled, result, samples);
+            return result;
+        }
+    }
+}
---
+++ mRandomTests/SequenceTests.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiffTheFox.mRandom.Tests
+{
+    [TestClass]
+    public class SequenceTests
+    {
+        const int ITERATIONS = 800;
+
+        [TestMethod]
+        public void PickTest()
+        {
+            var source = new string[] { "This", "Is", "Some", "String", "Data", "123", "456" };
+
+            using (var r = new RandomGenerator(new SystemRandomRandomnessSource()))
+            {
+                for (int i = 0; i < ITERATIONS; i++)
+                {
+                    CollectionAssert.Contains(source, r.Pick(source));
+                }
+            }
+        }
+
+        [TestMethod]
+        public void ShuffleTest()
+        {
+            var source = new string[] { "This", "Is", "Some", "String", "Data", "123", "456" };
+
+            using (var r = new RandomGenerator(new SystemRandomRandomnessSource()))
+            {
+                for (int i = 0; i < ITERATIONS; i++)
+                {
+                    CollectionAssert.AreEquivalent(source, r.Shuffle(source));
+                }
+            }
+        }
+
+        [TestMethod]
+        public void SampleTest()
+        {
+            var source = new string[] { "This", "Is", "Some", "String", "Data", "123", "456" };
+
+            using (var r = new RandomGenerator(new SystemRandomRandomnessSource()))
+            {
+                for (int i = 0; i < ITERATIONS; i++)
+                {
+                    var samples = r.SampleWithoutReplacement(source, 3);
+                    CollectionAssert.AllItemsAreUnique(samples);
+                    foreach (var sample in samples)
+                    {
+                        CollectionAssert.Contains(source, sample);
+                    }
+                }
+            }
+        }
+    }
+}
